14ea6b0fcbb7bfc537f06464db342cb41182808c
Ismail <shaikhnagismail@gmail.com>
2023-07-31 13:18:29 +0200

    set up login

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -5,8 +5,10 @@ import LoginForm from "@/components/forms/LoginForm";
 
 const Login = () => {
   return (
-    <div className="flex justify-center items-center">
-      <Image src={logo} alt="logo" />
+    <div className="flex h-screen flex-col items-center justify-center gap-8">
+    
+        <Image src={logo} height={125} width={"auto"} alt="logo" />
+   
 
       <LoginForm />
     </div>
--- components/forms/LoginForm.jsx
+++ components/forms/LoginForm.jsx
@@ -14,7 +14,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
-const LoginForm = ({title, description}) => {
+const LoginForm = ({ title, description }) => {
   // 1. Initialize the useForm hook and get form methods and state
   const { register, handleSubmit, formState, setError, reset } = useForm();
 
@@ -23,73 +23,71 @@ const LoginForm = ({title, description}) => {
   };
 
   return (
-    <div className="flex h-full items-center justify-center">
-      <Card className="w-full max-w-md">
-        <CardHeader className="space-y-1">
-          <CardTitle className="text-2xl">Create an account</CardTitle>
-          <CardDescription>
-            Enter your email below to create your account
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="grid gap-4">
-          {/* email */}
-          <div className="grid gap-2">
-            <Label htmlFor="email" className="text-base font-semibold">
-              Email
-            </Label>
-            <Input
-              {...register("email", {
-                required: "Email is required",
-                pattern: {
-                  value: /\S+@\S+\.\S+/,
-                  message: "Entered value does not match email format",
-                },
-              })}
-              type="email"
-              id="email"
-              placeholder="m@example.com"
-              className="h-14 text-base placeholder:text-gray-400"
-            />
-            {/* Show validation error message */}
-            {formState.errors.email && (
-              <span className="mt-1 text-sm text-red-600">
-                {formState.errors.email.message}
-              </span>
-            )}
-          </div>
-          {/* password */}
-          <div className="grid gap-2">
-            <Label htmlFor="password" className="text-base font-semibold">
-              Password
-            </Label>
-            <Input
-              {...register("password", {
-                required: "Password is required",
-                minLength: {
-                  value: 4,
-                  message: "Password must have at least 4 characters",
-                },
-              })}
-              type="password"
-              id="password"
-              placeholder="create password"
-              className="h-14 text-base placeholder:text-gray-400"
-            />
-            {/* Show validation error message */}
-            {formState.errors.password && (
-              <span className="mt-1 text-sm text-red-600">
-                {formState.errors.password.message}
-              </span>
-            )}
-          </div>
-        </CardContent>
-        <CardFooter>
-          <Button onClick={handleSubmit(onSubmit)} className="w-full">
-            Create account
-          </Button>
-        </CardFooter>
-      </Card>
-    </div>
+    <Card className="w-full max-w-md">
+      <CardHeader className="space-y-1">
+        <CardTitle className="text-2xl">Log into your account</CardTitle>
+        <CardDescription>
+          Enter your email and password below to log in
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="grid gap-4">
+        {/* email */}
+        <div className="grid gap-2">
+          <Label htmlFor="email" className="text-[14px] ">
+            Email
+          </Label>
+          <Input
+            {...register("email", {
+              required: "Email is required",
+              pattern: {
+                value: /\S+@\S+\.\S+/,
+                message: "Entered value does not match email format",
+              },
+            })}
+            type="email"
+            id="email"
+            placeholder="m@example.com"
+            className="h-14 text-base placeholder:text-gray-400"
+          />
+          {/* Show validation error message */}
+          {formState.errors.email && (
+            <span className="mt-1 text-sm text-red-600">
+              {formState.errors.email.message}
+            </span>
+          )}
+        </div>
+        {/* password */}
+        <div className="grid gap-2">
+          <Label htmlFor="password" className="text-[14px]">
+            Password
+          </Label>
+          <Input
+            {...register("password", {
+              required: "Password is required",
+              minLength: {
+                value: 4,
+                message: "Password must have at least 4 characters",
+              },
+            })}
+            type="password"
+            id="password"
+            placeholder="enter your password"
+            className="h-14 text-base tracking-widest placeholder:tracking-normal placeholder:text-gray-400"
+          />
+          {/* Show validation error message */}
+          {formState.errors.password && (
+            <span className="mt-1 text-sm text-red-600">
+              {formState.errors.password.message}
+            </span>
+          )}
+        </div>
+      </CardContent>
+      <CardFooter>
+        <Button onClick={handleSubmit(onSubmit)} className="w-full">
+          Create account
+        </Button>
+      </CardFooter>
+    </Card>
   );
 };
 
